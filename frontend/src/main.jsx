1359a77e5f36c35c2e747577e8d29e2df72d8883
Sanidhya Soni <sanidhyasn314@gmail.com>
2025-01-12 15:15:14 +0530

    add robots.txt for search engine indexing and update index.html for script loading

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Room from './Room.jsx'
-import { BrowserRouter, Routes , Route } from 'react-router-dom'
-import BotPage from './components/BotPage.jsx'
-import Bot from './Bot.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Room from "./Room.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BotPage from "./components/BotPage.jsx";
+import Bot from "./Bot.jsx";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <>
-    {/* <App /> */}
     <BrowserRouter>
       <Routes>
-       <Route path="/" element={<Room />} />
+        <Route path="/" element={<Room />} />
         <Route path="/game/:name/:roomId" element={<App />} />
         <Route path="/game/:name/bot-multiplayer" element={<Bot />} />
       </Routes>
-      </BrowserRouter>
-      </>,
-  {/* </StrictMode>, */}
-)
+    </BrowserRouter>
+  </>
+);
